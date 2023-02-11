8621c5b1590318d62e23c0ccd39173b242920531
nigeljanssens <njanssens@inlichtingenbureau.nl>
2023-02-11 19:12:24 +0100

    delete radix separator and just use div

--- src/components/separator/index.tsx
+++ src/components/separator/index.tsx
@@ -1,5 +1,3 @@
-import * as SeparatorPrimitive from '@radix-ui/react-separator'
-
 export default function Separator() {
-  return <SeparatorPrimitive.Root decorative={true} orientation="vertical" className="h-full w-[1px] bg-gray-900 " />
+  return <div className="h-full w-[1px] bg-gray-900 " />
 }
