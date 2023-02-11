e8627bac0b77419a31f90390a752737aa03bb692
nigeljanssens <njanssens@inlichtingenbureau.nl>
2023-02-11 19:06:23 +0100

    fix: navbar

--- src/components/separator/index.tsx
+++ src/components/separator/index.tsx
@@ -1,5 +1,5 @@
 import * as SeparatorPrimitive from '@radix-ui/react-separator'
 
 export default function Separator() {
-  return <SeparatorPrimitive.Root decorative={true} orientation="vertical" className="h-5 w-[1px] bg-gray-900 " />
+  return <SeparatorPrimitive.Root decorative={true} orientation="vertical" className="h-full w-[1px] bg-gray-900 " />
 }
