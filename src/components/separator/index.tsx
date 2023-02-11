55ce61415cf84422b60019330a15d87acfec3b0b
nigeljanssens <njanssens@inlichtingenbureau.nl>
2023-02-11 18:49:20 +0100

    basic project setup

---
+++ src/components/separator/index.tsx
@@ -0,0 +1,5 @@
+import * as SeparatorPrimitive from '@radix-ui/react-separator'
+
+export default function Separator() {
+  return <SeparatorPrimitive.Root decorative={true} orientation="vertical" className="h-5 w-[1px] bg-gray-900 " />
+}
